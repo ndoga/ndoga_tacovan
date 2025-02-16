0ee31cad0c036e5efd93c5ca5edc041822ca0417
Matteo Gandelli <111530378+ndoga@users.noreply.github.com>
2025-02-16 03:23:47 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,14 +1,21 @@
 ESX = nil
 ESX = exports["es_extended"]:getSharedObject()
 
+function debugPrintServer(...)
+    if Config and Config.Debug then
+        print(...)
+    end
+end
+
+
 RegisterNetEvent('vehicle:giveItem', function(item, label)
     local src = source
-    print("[SERVER] vehicle:giveItem triggerato per item: " .. item .. " (" .. label .. ") da source: " .. src)
+    debugPrintServer("[SERVER] vehicle:giveItem triggerato per item: " .. item .. " (" .. label .. ") da source: " .. src)
     local xPlayer = ESX.GetPlayerFromId(src)
     if xPlayer then
         xPlayer.addInventoryItem(item, 1)
         TriggerClientEvent('dream_notifiche:client:Alert', src, "Milano Full RP", "Hai ricevuto: " .. label, 3000, "info")
     else
-        print("[SERVER] Impossibile trovare il giocatore con id: " .. src)
+        debugPrintServer("[SERVER] Impossibile trovare il giocatore con id: " .. src)
     end
 end)
